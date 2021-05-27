c80d2151a360ce1da41a6a836fa3b3e0a56e4712
adham97 <adham8697@gmail.com>
2021-05-27 11:54:18 +0300

    Snack

---
+++ src/Snack.java
@@ -0,0 +1,25 @@
+public class Snack {
+	String name;
+	double price;
+	
+	Snack(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+ 	public String getName() {
+		return this.name;
+	}
+ 	
+ 	public void setName(String name) {
+ 		this.name = name;
+ 	}
+ 	
+ 	public double getPrice() {
+		return this.price;
+	}
+ 	
+ 	public void setPrice(double price) {
+ 		this.price = price;
+ 	}
+}
