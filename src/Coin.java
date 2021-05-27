015e5588f2de5e40ada1f2f6e391e239e38f1327
adham97 <adham8697@gmail.com>
2021-05-27 12:19:10 +0300

    coin

---
+++ src/Coin.java
@@ -0,0 +1,29 @@
+public class Coin {
+	double value;
+	
+	Coin(double value) {
+		if(checkCoin(value)) {
+			this.value = value;
+		} else {
+			System.out.println("Sorry, our dear customer.");
+			System.out.println("The Snack Machine only accepts 10c, 20c, 50c, 1$");
+		}
+	}
+	
+	boolean checkCoin(double value) {
+		return (value == 0.1 || value == 0.2 || value == 0.5 || value == 1);
+	}
+	
+	public void setCoin(double value) {
+		if(checkCoin(value)) {
+			this.value = value;
+		} else {
+			System.out.println("Sorry, our dear customer.");
+			System.out.println("The Snack Machine only accepts 10c, 20c, 50c, 1$");
+		}
+	}
+	
+	public double getCoin() {
+		return value;
+	}
+}
