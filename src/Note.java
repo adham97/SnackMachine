8b7d93ba27f43a089e96420bbaf4077f3507c575
adham97 <adham8697@gmail.com>
2021-05-27 12:33:31 +0300

    Note

---
+++ src/Note.java
@@ -0,0 +1,29 @@
+public class Note {
+	int value;
+	
+	Note(int value) {
+		if(checkNote(value)) {
+			this.value = value;
+		} else {
+			System.out.println("Sorry, our dear customer.");
+			System.out.println("The Snack Machine only accepts 20$ or 50$");
+		}
+	}
+	
+	boolean checkNote(int value) {
+		return (value == 20 || value == 50);
+	}
+	
+	public void setNote(int value) {
+		if(checkNote(value)) {
+			this.value = value;
+		} else {
+			System.out.println("Sorry, our dear customer.");
+			System.out.println("The Snack Machine only accepts 20$ or 50$");
+		}
+	}
+	
+	public int getCoin() {
+		return value;
+	}
+}
